737d3739b3fb5b1b0b5940e25a167f12662d3e9b
bhardwajankit1810 <bhardwajankit1810@gmail.com>
2026-09-08 21:50:38 +0530

    first commit

---
+++ project_halfadder.srcs/sim_1/new/tb_half_adder.v
@@ -0,0 +1,43 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 03.09.2026 18:54:36
+// Design Name: 
+// Module Name: tb_half_adder
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+`timescale 1ns / 1ps
+module tb_half_adder;
+reg a;
+    reg b;
+    wire sum;
+    wire carry;
+
+    half_adder uut (
+        .a(a),
+        .b(b),
+        .sum(sum),
+        .carry(carry)
+    );
+
+    initial begin
+        a = 0; b = 0; #20;
+        a = 0; b = 1; #20;
+        a = 1; b = 0; #20;
+        a = 1; b = 1; #20;
+        $finish;
+    end
+endmodule
